da67baf491d6f4de65875160b1f79b448ed72f06
Gaby Nieva <gaby.nieva@outlook.com>
2024-07-03 23:48:06 -0600

    Starting

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    #return 'Hello World'
+    return render_template('base.html')
