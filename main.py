ef8fa7e19c6df3edbcf9bbb1fa016ea39450151c
Gaby Nieva <gaby.nieva@outlook.com>
2024-07-05 00:23:18 -0600

    DB

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
+import psycopg2
 from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+class LoginForm (FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.route('/')
 def index():
     #return 'Hello World'
     return render_template('base.html')
+
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+
+@app.route('/libros')
+def libros():
+    # Connect to the database 
+    conn = psycopg2.connect(database="flask_db", 
+                            user="postgres", 
+                            password="root", 
+                            host="localhost", port="5432") 
+  
+    # create a cursor 
+    cur = conn.cursor() 
+  
+    # Select all products from the table 
+    cur.execute('''SELECT * FROM libros''') 
+  
+    # Fetch the data 
+    data = cur.fetchall() 
+  
+    # close the cursor and connection 
+    cur.close() 
+    conn.close() 
+  
+    return render_template('libros.html', data=data) 
+  
